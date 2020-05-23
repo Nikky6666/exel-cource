841b800a85e1dbe0039480909ac0c5aae27f6626
Nikky6666 <borbut.nika@mail.ru>
2020-05-23 21:14:41 +0300

    Finish base framework functional

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import {ExelComponent} from '@core/ExelComponent';
+
+export class Header extends ExelComponent {
+    static className = 'excel__header';
+    toHTML() {
+        return `
+<input type="text" class="input" value="Новая таблица"/>
+            <div>
+                <div class="button">
+                    <span class="material-icons">delete</span>
+                </div>
+                <div class="button">
+                    <span class="material-icons">exit_to_app</span>
+                </div>
+            </div>`;
+    }
+}
